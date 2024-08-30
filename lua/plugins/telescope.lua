5decf245f942294a5a22bd752217d8ba1aa95d2d
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2024-08-30 09:45:08 -0400

    Adding and configuring telescope project.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,18 +12,36 @@ return {
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-			--vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 		end
 	},
+--	{
+--		"nvim-telescope/telescope-file-browser.nvim",
+--		dependencies = {
+--			"nvim-telescope/telescope.nvim",
+--			"nvim-lua/plenary.nvim"
+--		},
+--		config = function()
+--			require("telescope").load_extension "file_browser" 
+--			vim.keymap.set("n", "<leader>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+--		end
+--	}
 	{
-		"nvim-telescope/telescope-file-browser.nvim",
+		"nvim-telescope/telescope-project.nvim",
 		dependencies = {
 			"nvim-telescope/telescope.nvim",
-			"nvim-lua/plenary.nvim"
 		},
 		config = function()
-			require("telescope").load_extension "file_browser" 
-			vim.keymap.set("n", "<leader>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+			require("telescope").load_extension("project")
+			require("telescope").setup({
+				extensions = {
+					project = {
+						sync_with_nvim_tree = true,
+					}
+				}
+			})
+			vim.keymap.set("n", "<leader>p", ":Telescope project<CR>")
 		end
+
 	}
 }
