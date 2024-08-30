16e9ad4026ace58f71a995301b467594919b552e
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2024-08-30 09:07:01 -0400

    Fixing config definition in colorscheme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,16 +3,16 @@ return {
 		"loctvl842/monokai-pro.nvim",
 		name = "monokai-pro",
 		devicons = true,
-		background_clear = {
-			"bufferline",
-			"lualine",
-			"nvim-tree",
-			"telescope"
-		},
 		config = function()
 			local monokai = require("monokai-pro")
 			monokai.setup({
-				filter = "classic"
+				filter = "classic",
+				background_clear = {
+					"bufferline",
+					"lualine",
+					"nvim-tree",
+					"telescope"
+				},
 			})
 			monokai.load()
 		end
