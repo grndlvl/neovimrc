d8dcdb442fbbc059fe4355332c4ad0be14c4827b
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2024-08-31 08:57:11 -0400

    Do better lazy loading on bufferline... and stay with the latest... I hope I don't regret this

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,12 +1,12 @@
 return {
   "akinsho/bufferline.nvim",
-  version = "4.7.0",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    require("bufferline").setup({})
-    vim.keymap.set("", "<C-Left>", ":bprevious<CR>")
-    vim.keymap.set("", "<C-Right>", ":bnext<CR>")
-  end,
+  opts = {},
+  event = "BufEnter",
+  keys = {
+    { "<C-Left>", ":bprevious<CR>", desc = "Buffer previous" },
+    { "<C-Right>", ":bnext<CR>", desc = "Buffer next" },
+  },
 }
