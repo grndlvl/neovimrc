6836f611fb1a5023f131647bac048b59ab69747d
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2024-08-31 15:58:27 -0400

    stylua cleanup.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,12 +16,8 @@ return {
   config = function()
     local cmp = require("cmp")
     local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities()
-    )
+    local capabilities =
+      vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
     local lspkind = require("lspkind")
 
     require("fidget").setup()
@@ -100,8 +96,7 @@ return {
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- For luasnip users.
-      },
-      {
+      }, {
         { name = "buffer" },
       }),
       formatting = {
