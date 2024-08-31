f4384f647d8b4bc4c218c9dad63608f21330370c
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2024-08-31 15:57:56 -0400

    Adding lspkind for nice symbols in autocomplete.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,32 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
     "j-hui/fidget.nvim",
+    "L3MON4D3/LuaSnip",
+    "onsails/lspkind.nvim",
+    "saadparwaiz1/cmp_luasnip",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
   },
   config = function()
     local cmp = require("cmp")
     local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities =
-      vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities()
+    )
+    local lspkind = require("lspkind")
 
-    require("fidget").setup({})
+    require("fidget").setup()
     require("mason").setup()
+
     require("mason-lspconfig").setup({
       ensure_installed = {
         "lua_ls",
@@ -91,17 +98,15 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
       }),
       sources = cmp.config.sources({
-        {
-          name = "nvim_lsp",
-        },
-        {
-          name = "luasnip",
-        }, -- For luasnip users.
-      }, {
-        {
-          name = "buffer",
-        },
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
+      },
+      {
+        { name = "buffer" },
       }),
+      formatting = {
+        format = lspkind.cmp_format(),
+      },
     })
   end,
 }
