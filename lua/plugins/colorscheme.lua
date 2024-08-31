89b82b8f07fce16094649a89e43f2133e24f9883
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2024-08-31 09:36:21 -0400

    Making the colorscheme more lazy friendly.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,14 @@
 return {
-  {
-    "loctvl842/monokai-pro.nvim",
-    name = "monokai-pro",
-    devicons = true,
-    config = function()
-      local monokai = require("monokai-pro")
-      monokai.setup({
-        filter = "classic",
-        background_clear = {
-          "bufferline",
-          "lualine",
-          "nvim-tree",
-          "telescope",
-        },
-      })
-      monokai.load()
-    end,
+  "loctvl842/monokai-pro.nvim",
+  name = "monokai-pro",
+  devicons = true,
+  opts = {
+    filter = "classic",
+    background_clear = {
+      "bufferline",
+      "lualine",
+      "nvim-tree",
+      "telescope",
+    },
   },
 }
