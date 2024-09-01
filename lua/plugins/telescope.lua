a61ba44f28524ed0210dc4d14d41a4f31005544f
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2024-09-01 08:35:57 -0400

    Make sure telescope loads lazily.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,15 +4,13 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    config = function()
-      require("telescope").setup({})
-
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files using Telescope" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep files using Telescope" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Search help tags using Telescope" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "List buffers in Telescope" })
-    end,
+    opts = {},
+    keys = {
+      { "<leader>ff", ":Telescope find_files<CR>", desc = "Find files using Telescope" },
+      { "<leader>fg", ":Telescope live_grep<CR>", desc = "Grep files using Telescope" },
+      { "<leader>fh", ":Telescope help_tags<CR>", desc = "Search help tags using Telescope" },
+      { "<leader>fb", ":Telescope buffers<CR>``", desc = "List buffers in Telescope" },
+    },
   },
   {
     "nvim-telescope/telescope-project.nvim",
