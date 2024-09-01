0bc93163ad13c708fb99fa9830745b509642d5f8
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2024-09-01 11:48:13 -0400

    Make floating terminal larger.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,10 +2,10 @@ return {
   "akinsho/toggleterm.nvim",
   event = "VeryLazy",
   opts = {
-    float_opts = {
-      width = 100,
-      height = 20,
-    },
+    --    float_opts = {
+    --      width = 100,
+    --      height = 20,
+    --    },
   },
   keys = {
     { "<leader>t", ":ToggleTerm direction=float<CR>", desc = "Open a floating terminal at the current directory" },
