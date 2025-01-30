fb3f5d029cb92e29b65a8ba7ff60a25efa19efd8
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-01-30 09:38:25 -0500

    Disable bufferline.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,12 +1,12 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  opts = {},
-  event = "BufEnter",
-  keys = {
-    { "<C-Left>", ":bprevious<CR>", desc = "Buffer previous" },
-    { "<C-Right>", ":bnext<CR>", desc = "Buffer next" },
-  },
+  --  "akinsho/bufferline.nvim",
+  --  dependencies = {
+  --    "nvim-tree/nvim-web-devicons",
+  --  },
+  --  opts = {},
+  --  event = "BufEnter",
+  --  keys = {
+  --    { "<C-Left>", ":bprevious<CR>", desc = "Buffer previous" },
+  --    { "<C-Right>", ":bnext<CR>", desc = "Buffer next" },
+  --  },
 }
