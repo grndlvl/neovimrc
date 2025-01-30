4b8e9fae5e2142492910621b6db9edc555092360
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-01-30 09:40:40 -0500

    Pickup Drupal modules as PHP.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -69,6 +69,7 @@ return {
               ["indexer.include_patterns"] = {
                 "/**/*.php",
                 "/**/*.inc",
+                "/**/*.module",
               },
             },
           })
