abaaa8e8e12a30893e6ccf49437fe758fd3a0747
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-01-30 10:17:13 -0500

    Fixing PHP indenting.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,8 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    -- Disable default smartindent since we're using treesitters indenting.
-    vim.opt.smartindent = false
+    -- Enable default smartindent since we're not using treesitters indenting.
+    vim.opt.smartindent = true
 
     require("nvim-treesitter.configs").setup({
       -- A list of parser names, or "all"
@@ -38,20 +38,18 @@ return {
       auto_install = true,
 
       indent = {
-        enable = true,
+        -- Use default smartindent as this is really buggy... especially with
+        -- PHP heredoc.
+        enable = false,
       },
 
       highlight = {
-        -- `false` will disable the whole extension
         enable = true,
-
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = {
-          "markdown",
-        },
+        additional_vim_regex_highlighting = true,
       },
     })
   end,
