0bb816cfea9328e3bea2565bbaefd4127812f45d
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2025-01-30 09:38:56 -0500

    Use ntrw using old nerdtree keybinding.

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,4 +4,4 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("n", "<leader>fv", vim.cmd.Ex, { desc = "List files using netrw" })
+vim.keymap.set("n", "\\n", vim.cmd.Ex, { desc = "List files using netrw" })
